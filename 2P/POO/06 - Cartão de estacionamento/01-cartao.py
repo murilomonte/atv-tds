de3ac83bc295b789cf0baba9379979f79e64c8f1
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2024-12-03 07:18:25 -0300

    feat: atividade cartão estacionamento

--- 2P/POO/06 - Cartão de estacionamento/01-cartao.py
+++ 2P/POO/06 - Cartão de estacionamento/01-cartao.py
@@ -30,11 +30,31 @@ class CartaoEstac:
         self.__numC = self.__get_numCartao() # alfanum 8 char
         self.__statusC = 'Ativo' # ativo or finalizado
         self.__entrada = self.__val_dataHora(entrada)
-        self.saida = None
+        self.__saida = None
         self.__tarifa = 4 # 4 reais por hora; 0.50 para cada 15min quando acima de 2h
+    
+    @property
+    def numC(self):
+        return self.__numC
+    
+    @property
+    def statusC(self):
+        return self.__statusC
+
+    @property
+    def tarifa(self):
+        return self.__tarifa
+    
+    @property
+    def entrada(self):
+        return self.__entrada
+    
+    @property
+    def saida(self):
+        return self.__saida
 
     def __str__(self) -> str:
-        return f'## Cartão de Estacionamento\nNúmero ID: {self.__numC}\nStatus: {self.__statusC}\nTarifa: R${self.tarifa * 2}/2 horas\nEntrada: {self.__entrada}\nSaida: {self.__saida}'
+        return f'## Cartão de Estacionamento\nNúmero ID: {self.__numC}\nStatus: {self.__statusC}\nTarifa: R${self.__tarifa * 2} p/ 2 horas\nEntrada: {self.__entrada}\nSaida: {self.__saida}\n'
     
     def __get_numCartao(self):
         digitos = ''.join(choices(string.digits, k=5))
@@ -50,23 +70,24 @@ class CartaoEstac:
         
     def registrar_saida(self, saida):
         self.consultar_valor(saida)
+        self.__saida = saida
         self.__statusC = 'Finalizado'
-        print(self.__str__)
+        print(self.__str__())
         print('Volte sempre!')
     
     def consultar_valor(self, saida):
         valDoRestante, valTotal = self.__get_valor(saida)
         if valDoRestante == 0:
-            print(f'## Consulta\nTarifa: R${self.__tarifa}\nTotal a pagar: R${valTotal}')
+            print(f'## Consulta\nHora: {saida}\nTarifa: R${self.__tarifa}\nTotal a pagar: R${valTotal}')
         else:
-            print(f"## Consulta:\nTarifa: R${self.__tarifa}\nValor por tempo excedente: R${valDoRestante}\nTotal a pagar: R${valTotal}")
+            print(f"## Consulta:\nHora: {saida}\nTarifa: R${self.__tarifa}\nValor por tempo excedente: R${valDoRestante}\nTotal a pagar: R${valTotal}\n")
  
     def __get_valor(self, saida):
         saida = self.__val_dataHora(saida)
-        if saida.hour <= self.__entrada.hour:
+        if saida <= self.__entrada:
             raise ValueError("Data e hora inválidas!")
         
-        totalMin = (saida.hour - self.__entrada.hour).total_seconds() / 60
+        totalMin = (saida - self.__entrada).total_seconds() / 60
         
         if totalMin > 120:
             minRestante = (totalMin - 120)
@@ -75,12 +96,13 @@ class CartaoEstac:
         else:
             valDoRestante = 0
             valTotal = self.__tarifa * 2
-        return valDoRestante, valTotal
+        return round(valDoRestante, 2), round(valTotal, 2)
 
 def main():
-    cartao = CartaoEstac("12/07/2024 20:23") #20:27
+    cartao = CartaoEstac("12/07/2024 20:23")
     # print(cartao)
-    cartao.set_saida("13/07/2024 20:24")
+    cartao.consultar_valor("13/07/2024 00:25")
+    cartao.registrar_saida("13/07/2024 01:25")
 
 if __name__ == "__main__":
     main()
--- 2P/POO/06 - Cartão de estacionamento/main.py
+++ 2P/POO/06 - Cartão de estacionamento/main.py
@@ -7,17 +7,19 @@ def __val_dataHora(momento, formato="%d/%m/%Y %H:%M"):
         except ValueError:
             raise ValueError("Data e hora inválidas!")
         
-entrada = __val_dataHora("12/07/2024 20:23")
-saida = __val_dataHora("12/07/2024 23:23")
+entrada = __val_dataHora("12/07/2024 20:25")
+saida = __val_dataHora("13/07/2024 20:25")
 
 total = (saida - entrada).total_seconds() / 60
+
+print(total)
 
 if total > 120:
     restante = (total - 120) / 15
     totalPag = (restante * 0.5) + 8
 else:
     totalPag = 8
-
 print(totalPag)
+
 # print('hora', total // 60)
 # print('minuto', total % 60)
